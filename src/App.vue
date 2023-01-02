7f1e68e7162e41256dffe8369feb9a675667d5be
vlbn <cf.valbuena@gmail.com>
2023-01-02 22:42:59 +0100

    i exist

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hc">🏨&nbsp;Host&nbsp;<strong>Central</strong></div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap");
+
+.hc {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  min-height: 100vh;
+  vertical-align: middle;
+  align-items: center;
+}
+
+body,
+html {
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  overflow-x: hidden !important;
+  margin: 0px;
+  padding: 0px;
+  font-weight: 400;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+img {
+  border: 0 !important;
+  -webkit-user-drag: none;
+  -khtml-user-drag: none;
+  -moz-user-drag: none;
+  -o-user-drag: none;
+  -user-drag: none;
+}
+
+* {
+  scrollbar-width: auto;
+  scrollbar-color: #a6a6a65a #ffffff;
+}
+
+*::-webkit-scrollbar {
+  width: 10px;
+}
+
+*::-webkit-scrollbar-track {
+  background: #ffffff;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: #a6a6a65a;
+  border-radius: 10px;
+  border: 3px solid #ffffff;
+}
+</style>
