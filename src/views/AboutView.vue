126023413d5b422d8cd57b26a723d9153d43c4cd
vlbn <valbuena@duck.com>
2023-02-12 22:55:55 +0100

    nice things pt1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -28,11 +28,31 @@ import router from "@/router";
     ratione asperiores amet illum, cupiditate nobis quod ab, soluta, quasi rerum nulla ut
     quisquam perspiciatis dolores animi quae inventore?
   </h1>
+  <div class="m-6">
+    <button class="button-82-pushable" role="button">
+      <span class="button-82-shadow"></span>
+      <span class="button-82-edge"></span>
+      <span class="button-82-front text"> Button 82 </span>
+    </button>
+  </div>
+  <div class="niceBox-2 p-6 m-6">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum neque illo, aut eos
+    eligendi nobis maxime sapiente mollitia tempore? Eius officiis necessitatibus modi.
+    Similique nihil voluptates hic qui, totam nulla?
+  </div>
+  <div class="niceBox-3 p-6 m-6">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum neque illo, aut eos
+    eligendi nobis maxime sapiente mollitia tempore? Eius officiis necessitatibus modi.
+    Similique nihil voluptates hic qui, totam nulla?
+  </div>
   <h1>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repudiandae veniam
     ratione asperiores amet illum, cupiditate nobis quod ab, soluta, quasi rerum nulla ut
     quisquam perspiciatis dolores animi quae inventore?
   </h1>
+  <button class="button-29" role="button">Button 29</button>
+  <button class="button-83" role="button">Button 83</button>
+  <button class="button-83" role="button">Button 82</button>
   <h1>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repudiandae veniam
     ratione asperiores amet illum, cupiditate nobis quod ab, soluta, quasi rerum nulla ut
