a59016c735f952edcb28986b8a4d19b76a7c5f62
vlbn <valbuena@duck.com>
2023-02-20 23:16:11 +0100

    nice photo

--- src/components/media/NicePhoto.vue
+++ src/components/media/NicePhoto.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const props = defineProps(["path"]);
+
+let loading = ref(true);
+let imageSrc = ref(null);
+let imageAlt = ref(null);
+
+onMounted(() => {
+  const img = new Image();
+  img.src = props.path;
+  img.onload = () => {
+    imageSrc.value = img.src;
+    loading.value = false;
+    imageAlt.value = img.src;
+  };
+});
+</script>
+
 <template>
-  <img src="@/assets/media/carina.jpg" alt="carina star system" />
+  <Transition name="fade">
+    <div v-if="loading" class="cargando">loading..</div>
+    <div v-else>
+      <img :src="imageSrc" :alt="imageAlt" :title="imageAlt" />
+    </div>
+  </Transition>
 </template>
