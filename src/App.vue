5a976d0317e98487c74384e9f9cfd5223d0df8c1
vlbn <valbuena@duck.com>
2023-02-20 12:55:40 +0100

    gg router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
-import { RouterView } from "vue-router";
+import { defineAsyncComponent, shallowRef } from "vue";
+
 import TheHeader from "./components/layout/TheHeader.vue";
 import TheFooter from "./components/layout/TheFooter.vue";
+
+const linkMagico = defineAsyncComponent(() =>
+  import("./components/auth/AuthMagicLink.vue")
+);
+
+const nicePic = defineAsyncComponent(() => import("./components/media/NicePhoto.vue"));
+
+const componenteActivo = shallowRef(linkMagico);
 </script>
 
 <template>
   <TheHeader />
-  <RouterView />
+  <component :is="componenteActivo"></component>
   <TheFooter />
 </template>
