da2574eeb0d4fd87da4a82c013146a9f7de7a805
vlbn <valbuena@duck.com>
2023-04-17 21:36:56 +0200

    less scss n more gsap

--- FancyMouseIcon.vue
+++ FancyMouseIcon.vue
@@ -1,102 +1,110 @@
 <script setup>
+import { ref, onMounted, watch } from "vue";
+import { gsap } from "gsap";
+
 const props = defineProps({
-    position: {
-        type: String,
-        default() {
-            return "";
-        },
-    },
+  colorA: {
+    type: String,
+    default: "#42b983",
+  },
+  colorB: {
+    type: String,
+    default: "#ffffff",
+  },
+  scrollWheelDir: {
+    default: false,
+  },
 });
+
+let colorA_ = ref(props.colorA);
+let colorB_ = ref(props.colorB);
+
+// mouse-wheel TimeLine instance
+let mwTl = ref();
+
+onMounted(() => {
+  // initial values
+  gsap.set(".mouse-wheel", {
+    background: colorA_.value,
+    autoAlpha: 0,
+    y: 0,
+  });
+  gsap.set(".mouse-body", {
+    background: colorB_.value,
+  });
+
+  // mouse-wheel TimeLine animation
+  mwTl = gsap
+    .timeline({ repeat: -1 })
+    .to(".mouse-wheel", {
+      duration: 0.5,
+      scale: 1.9,
+      autoAlpha: 1,
+    })
+    .to(
+      ".mouse-wheel",
+      {
+        duration: 0.5,
+        scale: 0.9,
+        y: 12,
+      },
+      0.3
+    )
+    .to(".mouse-wheel", {
+      autoAlpha: 0,
+    })
+    .to(".mouse-wheel", {
+      y: 0,
+      scale: 0,
+    });
+
+  //
+});
+
+let isUserScrolling = ref(props.scrollWheelDir);
+
+watch(isUserScrolling, () => {
+  if (isUserScrolling.value) {
+    mwTl.reverse(0);
+  } else {
+    mwTl.reverse();
+    mwTl.play(0);
+  }
+});
+
+//
 </script>
 
 <template>
-    <div :class="position" class="onTopOfEverything">
-        <div class="mouse-container pointer">
-            <div class="mouse-body">
-                <div class="mouse-wheel"></div>
-            </div>
-        </div>
+  <div class="mouse-container">
+    <div class="mouse-body">
+      <div class="mouse-wheel"></div>
     </div>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-// mouse icon - scroll indicator
-.onTopOfEverything {
-    z-index: 9999 !important;
+<style scoped>
+.mouse-container {
+  margin: 3rem;
+  width: 25px;
+  height: 40px;
 }
-.mouse {
-    &-container {
-        margin: 3rem;
-        width: 25px;
-        height: 43px;
-        z-index: 3;
-    }
-
-    &-body {
-        display: block;
-        position: relative;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        border: 2px solid #6e6e6e5e;
-        -moz-border-radius: 15px;
-        -webkit-border-radius: 15px;
-        border-radius: 15px;
-    }
-
-    &-wheel {
-        display: block;
-        margin: 0 0 0 -2px;
-        position: relative;
-        top: 10px;
-        left: 50%;
-        width: 4px;
-        height: 4px;
-        background: #6e6e6e5e;
-        -moz-border-radius: 50%;
-        -webkit-border-radius: 50%;
-        border-radius: 50%;
-        -moz-animation: mouse-wheel 1.8s linear 0s infinite normal none;
-        -webkit-animation: mouse-wheel 1.8s linear 0s infinite normal none;
-        animation: mouse-wheel 1.8s linear 0s infinite normal none;
-    }
+.mouse-body {
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  border: 0.1px solid #2e2e2e44;
+  -moz-border-radius: 15px;
+  -webkit-border-radius: 15px;
+  border-radius: 15px;
 }
 
-@keyframes mouse-wheel {
-    0% {
-        -moz-transform: translate3d(0, 0, 0);
-        -webkit-transform: translate3d(0, 0, 0);
-        transform: translate3d(0, 0, 0);
-        opacity: 1;
-    }
-
-    35% {
-        -moz-transform: translate3d(0, 7px, 0);
-        -webkit-transform: translate3d(0, 7px, 0);
-        transform: translate3d(0, 7px, 0);
-        opacity: 1;
-    }
-
-    50% {
-        -moz-transform: translate3d(0, 10px, 0);
-        -webkit-transform: translate3d(0, 10px, 0);
-        transform: translate3d(0, 10px, 0);
-        opacity: 0;
-    }
-
-    50.001% {
-        -moz-transform: translate3d(0, 0, 0);
-        -webkit-transform: translate3d(0, 0, 0);
-        transform: translate3d(0, 0, 0);
-        opacity: 0;
-    }
-
-    80% {
-        -moz-transform: translate3d(0, 0, 0);
-        -webkit-transform: translate3d(0, 0, 0);
-        transform: translate3d(0, 0, 0);
-        opacity: 0;
-    }
+.mouse-wheel {
+  margin-top: 0.3rem;
+  width: 5px;
+  height: 5px;
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
 }
 </style>
