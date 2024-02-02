9f3f013be364558a1edbae84f5964d6ffa19ca17
vlbn <valbuena@duck.com>
2024-02-02 18:40:25 +0100

    far from fancy yet GG bulma classes

--- demo/src/components/FancyPreLoader.vue
+++ demo/src/components/FancyPreLoader.vue
@@ -1,14 +1,21 @@
 <template>
-    <div class="is-flex is-justify-content-center fv-100 is-flex-direction-column is-align-items-center p-5">
+    <div class="basic">
         <div class="iSpin"></div>
     </div>
 </template>
 
 <style scoped>
+.basic {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column wrap;
+    min-height: 100vh;
+}
 .iSpin {
     &::after {
         animation: spinAround 200ms infinite linear;
-        border: 2px solid rgb(224, 176, 31);
+        border: 2px solid rgb(224, 31, 105);
         border-radius: 9999px;
         border-right-color: transparent;
         border-top-color: transparent;
