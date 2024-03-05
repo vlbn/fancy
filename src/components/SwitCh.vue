f056a4ecb50939b2c035cd66a952122fab615b88
valbuena <valbuena@duck.com>
2024-03-05 18:16:32 +0100

    component in progress

---
+++ src/components/SwitCh.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+    isOn: {
+        type: Boolean,
+    },
+});
+
+</script>
+
+<template>
+    <div>
+        {{ isOn }}
+    </div>
+</template>
+
+<style scoped>
+div {
+    cursor: pointer;
+}
+</style>
